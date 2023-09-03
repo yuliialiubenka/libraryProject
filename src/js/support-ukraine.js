d38303e0e9851c23bc31fce7c40680e26db33d12
Yuliia Liubenka <yuliialiubenka@gmail.com>
2023-09-03 18:17:08 +0300

    support-ukraine

--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -1,95 +1,78 @@
-// import Swiper from 'swiper';
-import Swiper from 'swiper';
+import { funds } from './funds';
 
-import found1 from '../images/foundation-1.png';
-import found2 from '../images/foundation-2.png';
-import found3 from '../images/foundation-3.png';
-import found4 from '../images/foundation-4.png';
-import found5 from '../images/foundation-5.png';
-import found6 from '../images/foundation-6.png';
-import found7 from '../images/foundation-7.png';
-import found8 from '../images/foundation-8.png';
-import found9 from '../images/foundation-9.png';
 
 function createFundsMarkup(funds) {
-  const markup = funds
-    .map((fund, index) => {
-      const foundIndex = String(index + 1).padStart(2, '0');
-      const foundImage = fund.img;
-      return `
-        <div class="funds__carousel-item swiper-slide">
-          <span class="funds__carousel-item-number">${foundIndex}</span>
-          <a class='funds__carousel-item-link' href="${fund.url}" target="_blank" crossorigin="anonymous" rel="noopener noreferrer nofollow" aria-label="${fund.title}">
-            <img class="funds__carousel-item-img" src="${foundImage}" alt="${fund.title}" loading="lazy">
-          </a>
-        </div>
-      `;
-    })
-    .join('');
+  const markup = funds.map((fund, index) => {
+    const foundIndex = String(index + 1).padStart(2, '0');
+
+    return `
+      <div class="funds__carousel-item swiper-slide">
+        <span class="funds__carousel-item-number">${foundIndex}</span>
+        <a class='funds__carousel-item-link' href="${fund.url}" target="_blank" crossorigin="anonymous" rel="noopener noreferrer nofollow" aria-label="${fund.title}">
+          <img class="funds__carousel-item-img" src="${fund.img}" alt="${fund.title}" loading="lazy" height="32">
+        </a>
+      </div>
+    `;
+  })
+  .join('');
   return markup;
 }
 
-const funds = [
-  {
-    title: 'Save the Children',
-    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-    img: found1,
-  },
-  {
-    title: 'Project HOPE',
-    url: 'https://www.projecthope.org/country/ukraine/',
-    img: found2,
-  },
-  {
-    title: 'International Medical Corps',
-    url: 'https://internationalmedicalcorps.org/country/ukraine/',
-    img: found3,
-  },
-  {
-    title: 'RAZOM',
-    url: 'https://www.razomforukraine.org/',
-    img: found4,
-  },
-  {
-    title: 'Action against hunger',
-    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-    img: found5,
-  },
-  {
-    title: 'Serhiy Prytula Charity Foundation',
-    url: 'https://prytulafoundation.org/en',
-    img: found6,
-  },
-  {
-    title: 'UNITED24',
-    url: 'https://u24.gov.ua/uk',
-    img: found7,
-  },
-  {
-    title: 'World vision',
-    url: 'https://www.wvi.org/emergencies/ukraine',
-    img: found8,
-  },
-  {
-    title: 'Medicins Sans   Frontieres',
-    url: 'https://www.msf.org/ukraine',
-    img: found9,
-  },
-];
-
-const container = document.querySelector('#fundsList');
-container.insertAdjacentHTML(
+const fundsContainer = document.querySelector('#fundsList');
+fundsContainer.insertAdjacentHTML(
   'beforeend',
   createFundsMarkup(funds)
 );
 
-const fundsSwiper = new Swiper('.funds__carousel', {
-  direction: 'vertical',
-  loop: 'true',
-  slidesPerView: 6,
-  spaceBetween: 20,       
-  speed: 1000,
-  navigation: {
-    nextEl: '.funds__carousel-button.next',
-  },
-});
+const slider = $(".slider-item");
+
+slider.each(function(element) {
+  const $element = $(element);
+  $element.data('slider-length', $element.children().length);
+}).slick({    
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  arrows: true,
+  vertical: true,
+  centerPadding: 0,
+  mobileFirst: false,
+  swipeToSlide: true,
+  responsive: [
+    {	
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+}).on('afterChange', onSliderAfterChange).on('wheel', onSliderWheel);
+
+
+function onSliderAfterChange(event, currentSlide) {
+  $(event.target).data('current-slide', currentSlide);
+}
+
+function onSliderWheel(element) {
+  const deltaY = element.originalEvent.deltaY,
+    currentSlider = $(element.currentTarget),
+    currentSlickIndex = currentSlider.data('current-slide') || 0;
+     
+  if (
+    // check when you scroll up
+    (deltaY < 0 && currentSlickIndex == 0) ||
+    // check when you scroll down
+    (deltaY > 0 && currentSlickIndex == currentSlider.data('slider-length') - 1)
+  ) {
+    return;
+  }
+
+  element.preventDefault();
+
+  if (element.originalEvent.deltaY < 0) {
+    currentSlider.slick('slickPrev');
+  } else {
+    currentSlider.slick('slickNext');
+  }
+}
